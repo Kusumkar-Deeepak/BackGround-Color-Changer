0e50504f395dd840a504fc988edfd6755c4e5b5c
Kusumkar Deepak Prakash ❤ <150149158+Kusumkar-Deeepak@users.noreply.github.com>
2024-01-21 13:17:44 +0530

    Add files via upload
    This project is a basic HTML/CSS/JS application that provides a user interface with color options. Each color option, represented by a clickable div, changes the background color of the page dynamically.

---
+++ script.js
@@ -0,0 +1,22 @@
+function Red(){
+    document.body.style.background="red";
+}function Blue(){
+    document.body.style.background="Blue";
+}function orange(){
+    document.body.style.background="orangered";
+}function Yellow(){
+    document.body.style.background="Yellow";
+}function Grey(){
+}function darkmagenta(){
+    document.body.style.background="darkmagenta";
+}function Grey(){
+    document.body.style.background="Grey";
+}function dodgerblue(){
+    document.body.style.background="dodgerblue";
+}function turquoise(){
+    document.body.style.background="turquoise";
+}function teal(){
+    document.body.style.background="teal";
+}function gold(){
+    document.body.style.background="gold";
+}
